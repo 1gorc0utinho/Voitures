25ac27b741b55e54bd27c7c1dee4351c620c8af4
1gorc0utinho <109849489+1gorc0utinho@users.noreply.github.com>
2022-07-23 10:37:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function buttonBlack(){
+  document.body.style.backgroundColor = "black"
+}
+function buttonRed(){
+  document.body.style.backgroundColor = "#8C0303"
+}
+function buttonYellow(){
+  document.body.style.backgroundColor = "#D9851E"
+}
+function buttonBlue(){
+  document.body.style.backgroundColor = "#1C588C"
+}
+function buttonWhite(){
+  document.body.style.backgroundColor = "white"
+}
